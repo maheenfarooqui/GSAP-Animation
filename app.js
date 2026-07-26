12336f63dcf99f046ebbc01108d44c7f8c159f0d
maheen <maheenzuhra@gmail.com>
2026-07-26 15:03:38 +0500

    add animation

--- app.js
+++ app.js
@@ -9,14 +9,25 @@
 // yoyo:true
 
 gsap.from('#item', {
-      y: -100,                   // Screen ke kaafi upar se shuru hoga
-      duration: 2,              // Complete hone ka time
-      delay: 0.5,               // Kitni der baad shuru ho
+      y: -50,                   // Screen ke kaafi upar se shuru hoga
+      duration: 0.5,              // Complete hone ka time
+      delay: 0.1,               // Kitni der baad shuru ho
       backgroundColor: "#38bdf8",// Start color (Animates to pink)
       ease: "bounce.out",       // Realistic bounce effect!
       repeat: -1,               // Infinite loop
-      repeatDelay: 0.5, 
-      stagger:0.5,        // Har cycle ke baad thoda pause
+      repeatDelay: 0.1, 
+      stagger:0.05,        // Har cycle ke baad thoda pause
+      yoyo: true                // Wapas upar bhi jaye
+    });
+    gsap.from('#item1', {
+      y: -50,                   // Screen ke kaafi upar se shuru hoga
+      duration: 0.5,              // Complete hone ka time
+      delay: 0.1,               // Kitni der baad shuru ho
+      backgroundColor: "#38bdf8",// Start color (Animates to pink)
+      ease: "bounce.out",       // Realistic bounce effect!
+      repeat: -1,               // Infinite loop
+      repeatDelay: 0.1, 
+      stagger:0.05,        // Har cycle ke baad thoda pause
       yoyo: true                // Wapas upar bhi jaye
     });
 // })
